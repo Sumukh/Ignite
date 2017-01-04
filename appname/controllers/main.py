54e29e9c545c3d0898b3b7b700aad9fcdf525b79
Sumukh Sridhara <sumukh1@gmail.com>
2017-01-03 18:47:00 -0800

    Fix bug when cache used for main page

--- appname/controllers/main.py
+++ appname/controllers/main.py
@@ -1,12 +1,12 @@
 from flask import Blueprint, render_template
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 from appname.extensions import cache
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
-@cache.cached(timeout=1000)
+@cache.cached(timeout=1000, unless=lambda: current_user.is_authenticated)
 def home():
     return render_template('index.html')
 
