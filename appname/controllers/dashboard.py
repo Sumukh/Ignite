c4c03d33957a613fceeb58ff222446da9b3596d5
Sumukh Sridhara <sumukh1@gmail.com>
2017-11-23 15:43:37 -0800

    change password

--- appname/controllers/dashboard.py
+++ appname/controllers/dashboard.py
@@ -1,6 +1,8 @@
 from flask_login import login_required, current_user
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, redirect, url_for
+
+from appname.forms.login import ChangePasswordForm
 from appname.extensions import cache
 
 dashboard = Blueprint('dashboard', __name__)
@@ -13,5 +15,19 @@ def home():
 @dashboard.route('/dashboard/settings')
 @login_required
 def settings():
-    return render_template('dashboard/settings.html')
+    form = ChangePasswordForm()
+    return render_template('dashboard/settings.html', form=form)
 
+@dashboard.route('/dashboard/change_password', methods=['POST'])
+@login_required
+def change_password():
+    form = ChangePasswordForm()
+
+    if form.validate_on_submit():
+        user.password = form.password.data
+        db.session.commit()
+        flash("Changed password", "success")
+    else:
+        flash("The password was invalid", "warning")
+
+    return redirect(url_for("dashboard.settings"))
