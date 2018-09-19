1138fbd0bcf64d08edb1ba8f2433be4b70a07046
Sumukh Sridhara <sumukh1@gmail.com>
2018-09-19 10:36:53 -0700

    handle not having stripe

--- appname/controllers/main.py
+++ appname/controllers/main.py
@@ -1,3 +1,5 @@
+import stripe 
+
 from flask import Blueprint, render_template, redirect, url_for
 from flask_login import current_user
 
@@ -9,7 +11,7 @@ main = Blueprint('main', __name__)
 def home():
     if current_user.is_authenticated:
         return redirect(url_for('dashboard_home.index'))
-    return render_template('lander/index.html')
+    return render_template('lander/index.html', stripe_publishable_key=stripe.publishable_key)
 
 @main.route('/beta')
 @cache.cached(timeout=1000, unless=lambda: current_user.is_authenticated)
